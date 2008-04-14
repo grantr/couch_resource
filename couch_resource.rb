581e531c76e2e650a6cb7de5b542829382862628
Grant Rodgers <grant@nightriot.com>
2008-04-13 20:32:07 -0700

    load method that supports plain arrays (ActiveResource only supports arrays of hashes by default)

--- couch_resource.rb
+++ couch_resource.rb
@@ -13,5 +13,23 @@ module CouchResource
       end
     end
 
+    def load(attributes)
+      raise ArgumentError, "expected an attributes Hash, got #{attributes.inspect}" unless attributes.is_a?(Hash)
+      @prefix_options, attributes = split_options(attributes)
+      attributes.each do |key, value|
+        @attributes[key.to_s] =
+          case value
+            when Array
+              resource = find_or_create_resource_for_collection(key)
+              value.map { |attrs| attrs.is_a?(Hash) ? resource.new(attrs) : (attrs.dup rescue attrs) }
+            when Hash
+              resource = find_or_create_resource_for(key)
+              resource.new(value)
+            else
+              value.dup rescue value
+          end
+      end
+      self
+    end    
   end
 end
